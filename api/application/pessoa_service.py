46d84b2254198162050dcfa1944cc255e8698d17
gigimach <112143871+gigimach@users.noreply.github.com>
2023-09-15 20:46:48 -0300

    Correção de erros

--- api/application/pessoa_service.py
+++ api/application/pessoa_service.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException, status
 from sqlmodel import Session, select
+from psycopg2.errors import UniqueViolation
 
 from persistence.utils import obter_engine
 from presentation.viewmodels.models import Pessoa
@@ -7,10 +8,21 @@ from presentation.viewmodels.models import Pessoa
 class PessoaService():
     def __init__(self):
         self.session = Session(obter_engine())
+
+
+    def obter_todas_pesssoas(self):
+        instrucao = select(Pessoa)
+        pessoas = self.session.exec(instrucao).fetchall()
+
+        return pessoas
+
     
-    # Esse Endpoint não funciona
     def criar_pessoa(self, pessoa: Pessoa):
         self.session.add(pessoa)
+
+        if UniqueViolation:
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Apelido já existe")
+
         self.session.commit()
         self.session.refresh(pessoa)
         self.session.close()
