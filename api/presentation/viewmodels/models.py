703f689bf50de22d20d345fe66cbb5c912c27710
gigimach <112143871+gigimach@users.noreply.github.com>
2023-09-14 23:19:13 -0300

    Correção de erros

--- api/presentation/viewmodels/models.py
+++ api/presentation/viewmodels/models.py
@@ -1,7 +1,7 @@
 from sqlmodel import Field, SQLModel, Date
 
 
-class Pessoa(SQLModel, table='True'):
+class Pessoa(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     apelido: str = Field(max_length=32, unique=True)
     nome: str = Field(max_length=100)
