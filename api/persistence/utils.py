b425a951ec69d1cdf5e9171eb25dd59ff682ff14
gigimach <112143871+gigimach@users.noreply.github.com>
2023-09-14 23:17:50 -0300

    Adição da url do BD

--- api/persistence/utils.py
+++ api/persistence/utils.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine
 
 
 def obter_engine():
-    db_url = ''
+    db_url = 'postgresql+psycopg2://pessoasdb_user:E8WioqKwy99EusBayVA9mo43x00xGgEV@dpg-ck1p2qn03lhc73fdm0e0-a.oregon-postgres.render.com/pessoasdb'
     engine = create_engine(db_url, echo=True)
 
     return engine
